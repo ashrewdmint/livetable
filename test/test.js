d7b30e10a30af698686e0bcdeb88522c40b20953
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-02-16 20:43:28 -0700

    Test for addType method

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 //  $.livetable
-//  
-//  - addType
+//
 //  - removeType
 //  - hasType
 //  - data
@@ -49,6 +48,30 @@
 
 $(document).ready(function(){
   var table = $('table');
+  
+  
+  module('$.livetable');
+  
+  test('addType', function(){
+    var func1 = function() { return 1; };
+    var func2 = function() { return 2; };
+    
+    equals(false, $.livetable.addType(), 'should return false if name is not a string');
+    equals(true,  $.livetable.addType('test'), 'should return true if name is a string');
+    equals(true,  $.livetable.addType('test2', func1, func2), 'should return true if a valid name and two functions are passed');
+    
+    var saved1    = $.livetable._types['test'];
+    var saved2    = $.livetable._types['test2'];
+    var expected1 = {to_field: null, to_text: null};
+    var expected2 = {to_field: func1, to_text: func2};    
+    
+    ok(saved1, 'should add first type to _types object');
+    ok(saved2, 'should add second type to _types object');
+    same(expected1, saved1, 'to_field and to_text properities should be null if no functions were passed');
+    same(expected2, saved2, 'to_field and to_text properities should be functions if functions were passed');
+  });
+  
+  
   
   module('Basic requirements');
   
